46ec0b0dcf7149cb70dd139238483ee345cebe9b
Alya Zouzou <alyazouzou@MacBook-Air-de-Alya.local>
2025-01-10 00:39:59 +0100

    started tracker implementation

---
+++ phase2/track.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+import numpy as np 
+
+class PlayerTracker:
+    def __init__(self,model_path="/Users/alyazouzou/Desktop/CV_Football/FootCVision2/phase1/runs/detect/train/weights/best.pt"):
+        pass
